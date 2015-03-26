02cab13727ba1ce78fbce2ce2f87b6afb31603b8
mizlin <you@example.com>
2015-03-26 17:48:39 +0100

    project folder added

---
+++ project/incl/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="sv">
+
+<body>
+<head>
+  <meta charset="utf-8">
+  <title><?php echo $pageTitle; ?></title>
+
+<!-- favicon bild -->
+   <link rel="shortcut icon" href="img/bom_favicon.png">
+   <link rel="stylesheet" href="style/stylesheet.css" title="stylesheet">
+
+<!-- Header med logga och navigareing -->
+
+  <header id="top">
+      <img src="img/top_img.png" alt="top image" id="top_img">
+   <!-- Navigeringsmeny -->
+   <nav class="navmenu">
+    <a id="startsida-" href="index.php">hem</a>
+    <a id="om_oss-" href="om_oss.php">om oss</a>
+       <a id="artiklar-" href="artiklar.php">artiklar</a>
+       <a id="objekt-" href="objekt.php">objekt</a>
+   </nav>
+  </header>
+
+
---
+++ project/index.php
@@ -0,0 +1,32 @@
+<?php
+    $pageTitle = "Välkommen till Begravningsmuseum Online";
+	include("incl/header.php");
+?>
+
+<!-- Sidans/Dokumentets huvudsakliga innehåll -->
+
+<div id="content">
+	<article class="justify border">
+		<h1>
+			<?php
+				echo $pageTitle;
+			?>
+		</h1>
+			<p>Begravningsmuseum Online (BMO) finns för att vårda ett kulturarv av seder och bruk kring begravningar. Vilka var de seder och bruk som användes vid begravningar för ett eller två sekel sedan? Vi glömmer snabbt. BMO finns för att vårda minnen av gångna tiders seder och bruk. Förhoppningsvis ger det oss en förståelse för hur det var på farfars och mormors tid, kanske kan det även hjälpa oss att nyansera vår bild av hur vi gör idag.
+
+			<p>Denna webbplats är resultat av ett samarbete mellan Ronny Holm, Mikael Roos och studenter på kursen "Databaser, HTML, CSS och skriptbaserad PHP-programmering" vid Blekinge Tekniska Högskola.
+
+			<p>Texterna på BMO är skapade av Ronny Holm och Mikael Roos, om inte annat anges. Bilderna som används är från Ronny Holms privata samling. De objekt som finns på BMO är från Ronny Holms privata samling om inget annat anges.
+
+			<p>Varje student har skapat ett eget BMO, mer eller mindre inspirerade av sina med-studenters arbeten, input från Ronny Holm i sin funktion som kund samt input och feedback från kursens lärare.
+
+			<p>Text och material på denna webbplats är publicerat enligt licensen Creative Commons Attribution Share-Alike License v3.0 om inte annat anges.
+
+	</article>
+</div> 	<!-- end of content -->
+
+
+<?php
+	include("incl/footer.php");
+?>
+
