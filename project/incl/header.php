6ad669a69dee6c29d7f944f758139c8a20158645
mizlin <you@example.com>
2015-03-27 08:00:33 +0100

    jobbar med layouten av startsidan

--- project/incl/header.php
+++ project/incl/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="sv">
 
-<body>
+<body<?php if(isset($pageId)) echo " id='$pageId' "; ?>>
 <head>
   <meta charset="utf-8">
   <title><?php echo $pageTitle; ?></title>
@@ -9,18 +9,23 @@
 <!-- favicon bild -->
    <link rel="shortcut icon" href="img/bom_favicon.png">
    <link rel="stylesheet" href="style/stylesheet.css" title="stylesheet">
+</head>
 
 <!-- Header med logga och navigareing -->
 
   <header id="top">
-      <img src="img/top_img.png" alt="top image" id="top_img">
+   <a href="index.php"><img src="img/top_img.png" alt="top image" id="top_img"></a>
+
+   <!-- login & logout menu -->
+   <div id="loginmenu"><?php echo userLoginMenu(); ?></div>
+
    <!-- Navigeringsmeny -->
-   <nav class="navmenu">
-    <a id="startsida-" href="index.php">hem</a>
-    <a id="om_oss-" href="om_oss.php">om oss</a>
-       <a id="artiklar-" href="artiklar.php">artiklar</a>
-       <a id="objekt-" href="objekt.php">objekt</a>
-   </nav>
+   <ul class="navmenu">
+    <li><a id="startsida-" href="index.php">hem</a></li>
+    <li><a id="om_oss-" href="om_oss.php">om oss</a></li>
+    <li><a id="artiklar-" href="artiklar.php">artiklar</a></li>
+    <li><a id="objekt-" href="objekt.php">objekt</a></li>
+   </ul>
   </header>
 
 
--- project/index.php
+++ project/index.php
@@ -1,5 +1,8 @@
 <?php
+	include("incl/config.php");
+
     $pageTitle = "Välkommen till Begravningsmuseum Online";
+    $pageId = "top";
 	include("incl/header.php");
 ?>
 
@@ -12,6 +15,9 @@
 				echo $pageTitle;
 			?>
 		</h1>
+
+		  <img class="img-float-left" src="img/ronny_holm.jpg" alt="Ronnt Holm">
+
 			<p>Begravningsmuseum Online (BMO) finns för att vårda ett kulturarv av seder och bruk kring begravningar. Vilka var de seder och bruk som användes vid begravningar för ett eller två sekel sedan? Vi glömmer snabbt. BMO finns för att vårda minnen av gångna tiders seder och bruk. Förhoppningsvis ger det oss en förståelse för hur det var på farfars och mormors tid, kanske kan det även hjälpa oss att nyansera vår bild av hur vi gör idag.
 
 			<p>Denna webbplats är resultat av ett samarbete mellan Ronny Holm, Mikael Roos och studenter på kursen "Databaser, HTML, CSS och skriptbaserad PHP-programmering" vid Blekinge Tekniska Högskola.
@@ -20,9 +26,16 @@
 
 			<p>Varje student har skapat ett eget BMO, mer eller mindre inspirerade av sina med-studenters arbeten, input från Ronny Holm i sin funktion som kund samt input och feedback från kursens lärare.
 
-			<p>Text och material på denna webbplats är publicerat enligt licensen Creative Commons Attribution Share-Alike License v3.0 om inte annat anges.
+			<p>Text och material på denna webbplats är publicerat enligt licensen <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution Share-Alike License v3.0</a> om inte annat anges.
 
+			<p><a href="om_oss.php">› Mer om oss</a>
 	</article>
+
+    <!-- byline -->
+	<div id="byline" class="byline">
+	 <p><strong>Om utvecklaren</strong></p>
+	 <p>Jag som har skapat hemsidan heter Mi. Utom html och php läser jag Python på BTH. Målet är att jag ska kunna jobba inom utvecklingsbranschen. På fritiden bakar jag kaffebröd och tårtor. Älskar djur, speciellt katter.</p>
+	 </div>
 </div> 	<!-- end of content -->
 
 
